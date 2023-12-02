cb4f1fe576640984a5580b2a8afc0f7a6919ee81
Sammers21 <titantins@gmail.com>
2023-12-02 17:45:22 +0300

    day 2 update

--- src/day2.rs
+++ src/day2.rs
@@ -1,15 +1,15 @@
 pub fn day2() {
-//     let test = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-// Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-// Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-// Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-// Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-
     let d2p1 = include_str!("../data/day2p1.txt");
-    // let d2p2 = include_str!("../data/day2p2.txt");
+    let d2p2 = include_str!("../data/day2p2.txt");
     println!("Day 2 Part 1: {}", cubes_ids_sum_part1(d2p1));
-    // println!("Day 2 Part 2: {}", checksum_of_document_part2(d2p2));\
-    // println!("Day 2 Part 1: {}", cubes_ids_sum_part1(test));
+    println!("Day 2 Part 2: {}", cubes_sum_of_pws_part2(d2p2));
+}
+
+fn cubes_sum_of_pws_part2(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|line| game_power(&line))
+        .sum()
 }
 
 fn cubes_ids_sum_part1(input: &str) -> i32 {
@@ -19,6 +19,62 @@ fn cubes_ids_sum_part1(input: &str) -> i32 {
         .filter(|game| game.1)
         .map(|game| game.0)
         .sum()
+}
+
+fn game_power(input: &str) -> i32 {
+    let mut red = -1;
+    let mut green = -1;
+    let mut blue = -1;
+    // split input by ':'
+    let mut game_split = input.split(':');
+    let _ = game_split.next().unwrap();
+    let game = game_split.next().unwrap();
+    // split input by ';'
+    let rounds = game.split(';');
+    // for each round calculate the sum of cubes
+    for round in rounds {
+        // split round by ','
+        // and trim each cube and get a vector of tuples (number, color)
+        let cubes = round.split(',')
+            .map(|cube| cube.trim())
+            .map(|cube| cube.split(' '))
+            .map(|cube| {
+                let mut cube = cube;
+                let number = cube.next().unwrap().parse::<i32>().unwrap();
+                let color = cube.next().unwrap().trim();
+                // println!("{} {}", number, color);
+                (number, color)
+            });
+        // group cubes by color
+        // group cubes by color
+        let mut red_cubes = 0;
+        let mut green_cubes = 0;
+        let mut blue_cubes = 0;
+        for cube in cubes {
+            match cube.1 {
+                "red" => red_cubes += cube.0,
+                "green" => green_cubes += cube.0,
+                "blue" => blue_cubes += cube.0,
+                col => panic!("Invalid color {}", col),
+            }
+        }
+        if red == -1 {
+            red = red_cubes;
+        } else if red < red_cubes {
+            red = red_cubes;
+        } 
+        if green == -1 {
+            green = green_cubes;
+        } else if green < green_cubes {
+            green = green_cubes;
+        }
+        if blue == -1 {
+            blue = blue_cubes;
+        } else if blue < blue_cubes {
+            blue = blue_cubes;
+        }
+    }
+    return  red * green * blue;
 }
 
 fn is_game_possible(input: &str) -> (i32, bool) {
